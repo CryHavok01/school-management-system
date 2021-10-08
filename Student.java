617304ca3dc46eccd2b17597b7ee76b045629306
Josh Halpern <jhhalpern@gmail.com>
2021-10-08 12:19:06 -0400

    student_class branch

--- Student.java
+++ Student.java
@@ -5,8 +5,13 @@ public class Student {
   private int feesTotal;
   private int feesPaid;
 
+  public Student(int id, String name, int grade) {
+    this.id = id;
+    this.name = name;
+    this.grade = grade;
+    this.feesTotal = 30000;
+  }
 
-  
   public static void main(String[] args) {
     
   }
