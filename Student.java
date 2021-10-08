3f36cef7cc61197e2a865ac4cf200d8718d1c7e6
Josh Halpern <jhhalpern@gmail.com>
2021-10-08 12:14:41 -0400

    initial commit

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+  private int id;
+  private String name;
+  private int grade;
+  private int feesTotal;
+  private int feesPaid;
+
+
+  
+  public static void main(String[] args) {
+    
+  }
+}
